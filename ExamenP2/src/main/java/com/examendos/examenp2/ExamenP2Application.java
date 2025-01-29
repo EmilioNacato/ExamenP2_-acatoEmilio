52c36dfa2f826e8e2ae307e3fce690116bdd638b
EmilioNacato <ejnacato@espe.edu.ec>
2025-01-29 11:25:48 -0500

    Inicializacion Proyecto Examen

---
+++ ExamenP2/src/main/java/com/examendos/examenp2/ExamenP2Application.java
@@ -0,0 +1,13 @@
+package com.examendos.examenp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenP2Application.class, args);
+    }
+
+}
